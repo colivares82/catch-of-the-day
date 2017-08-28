97390b003f1c69dfda67da9ffe7c34a8fd38b6f6
colivares82 <carlos.olivares.ve@gmail.com>
2017-08-28 23:25:46 +0200

    removing github pages

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import App from './components/App/App';
 import StorePicker from "./components/StorePicker/StorePicker";
 import NotFound from "./components/NotFound/NotFound";
 
-const repo = `/${window.location.pathname.split('/')[1]}`;
 const Root = () => {
   return (
-    <BrowserRouter basename={repo}>
+    <BrowserRouter>
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
